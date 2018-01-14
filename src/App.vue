446928efb121fb625e57b07c25f2fcf1fa84ed09
César Augusto <cesaraugustokg6@hotmail.com>
2018-01-14 00:32:27 -0200

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <section class="hero is-primary is-bold is-fullheight">
+      <!-- Hero head: will stick at the top -->
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item">
+                TODO APP com Vue JS
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="row">
+            <div class="columns">
+              <div class="column is-half is-offset-one-quarter">
+                <todo-card></todo-card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Hero footer: will stick at the bottom -->
+      <div class="hero-foot">
+        <div class="container">
+          <div class="content has-text-centered">
+            <p>
+              by
+              <a href="http://github.com/cesarsuriano">
+                <strong>César Augusto</strong>
+              </a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import TodoCard from "./components/TodoCard";
+export default {
+  name: "app",
+  components: {
+    TodoCard
+  },
+  data() {
+    return {
+      msg: "Hello, World!"
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
